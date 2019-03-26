88bc3d5c319a47c8e042f5bdfe92507f8001ca0a
Nicole Speirs <nickispeirs@gmail.com>
2019-03-26 14:15:13 -0500

    add sign in/sign out methods and username display: complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import RoomList from "./components/RoomList";
 import MessageList from "./components/MessageList";
-import User from "./User";
+import User from "./components/User";
 import * as firebase from "firebase";
 
 //Initialize Firebase
@@ -25,6 +25,12 @@ handleRoomSelect = room => {
   this.setState({ currentRoom: room});
 };
 
+setUser = user => {
+  this.setState({
+    user: user,
+  });
+}
+
   render() {
     return (
       <div className="App">
@@ -32,9 +38,9 @@ handleRoomSelect = room => {
           <h1>Bloc Chat</h1>
         </header>
         <main>
+          <User firebase={firebase} user={this.state.user} setUser={this.setUser} />
           <RoomList firebase={firebase}  handleRoomSelect={this.handleRoomSelect} />
           <MessageList firebase={firebase} currentRoom={this.state.currentRoom}  />
-          <User firebase={firebase} />
         </main>
       </div>
     );
--- src/components/User.js
+++ src/components/User.js
@@ -2,6 +2,12 @@ import React from "react";
 
 class User extends React.Component {
 
+  componentDidMount = () => {
+    this.props.firebase.auth().onAuthStateChanged( user => {
+      this.props.setUser(user);
+    });
+  }
+
   signIn = () => {
     const provider = new this.props.firebase.auth.GoogleAuthProvider();
     this.props.firebase.auth().signInWithPopup( provider );
@@ -14,16 +20,27 @@ class User extends React.Component {
 
   render() {
     return (
-      <div className="user">
-        <button
-        type="button"
-        className="sign-in"
-        onClick={this.signIn}
-        Sign In
-        >
-        </button>
-      </div>
-      <div className="sign-out">
+      <div>
+        <section className="user">
+          <button
+            type="button"
+            className="sign-in"
+            onClick={this.signIn}
+          >
+          Sign In
+          </button>
+          <button
+            type="button"
+            className="sign-out"
+            onClick={this.signOut}
+            >
+          Sign Out
+          </button>
+        </section>
+          Current User:
+          {this.props.user ? this.props.user.displayName : 'Guest'}
+        <section>
+        </section>
       </div>
     );
   }
