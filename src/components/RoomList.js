70894d4429a6ad60c838555a30ac738c57a6a262
Nicole Speirs <nickispeirs@gmail.com>
2019-03-26 22:49:32 -0500

    add css and messages association

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./RoomList.css";
 
-
 class RoomList extends React.Component {
   state = {
     rooms: [],
     newRoomName: "",
     creating: false
-  }
+  };
   roomsRef = this.props.firebase.database().ref("rooms");
   // this.handleChange = this.handleChange.bind(this);
   // this.createRoom = this.createRoom.bind(this);
-
 
   componentDidMount = () => {
     this.roomsRef.on("child_added", snapshot => {
@@ -19,58 +17,66 @@ class RoomList extends React.Component {
       room.key = snapshot.key;
       this.setState({ rooms: this.state.rooms.concat(room) });
     });
-  }
+  };
 
-
-  handleChange = (e) => {
+  handleChange = e => {
     this.setState({
       newRoomName: e.target.value
     });
-  }
+  };
 
-
-  createRoom = (e) => {
+  createRoom = e => {
     e.preventDefault();
     this.roomsRef.push({ name: this.state.newRoomName });
     this.setState({
       newRoomName: "",
       creating: false
     });
-  }
-
-
+  };
 
   render() {
     return (
       <div>
-        <ul>
+        <div>
+          <button className="button" onClick={e => this.setState({ creating: true })}>
+            New Room
+          </button>
+        </div>
+
+        {this.state.creating ? (
+          <div className="new-room">
+            <form onSubmit={e => this.createRoom(e)}>
+              <fieldset>
+                <legend>Create new room</legend>
+
+                <p>
+                  <label>Enter Room Name:</label>
+                  <input
+                    type="text"
+                    value={this.state.newRoomName}
+                    onChange={this.handleChange}
+                  />
+                </p>
+                <p>
+                  <input type="submit" value="submit" />
+                </p>
+              </fieldset>
+            </form>
+          </div>
+        ) : (
+          ""
+        )}
+        <ul className="room-list">
           {this.state.rooms.map(room => (
-            <li className="room-name" key={room.key} onClick={() => this.props.handleRoomSelect(room)}>
+            <li
+              className="room-name"
+              key={room.key}
+              onClick={() => this.props.handleRoomSelect(room)}
+            >
               {room.name}
             </li>
           ))}
         </ul>
-        <div>
-          <button onClick={(e)=>this.setState({creating:true})}>new room</button>
-        </div>
-
-        {(this.state.creating) ? <div className="new-room">
-          <form onSubmit={(e)=>this.createRoom(e)}>
-            <fieldset>
-              <legend>Create new room</legend>
-
-              <p>
-                <label>Enter Room Name:</label>
-                <input
-                type="text"
-                value={this.state.newRoomName}
-                onChange={this.handleChange}
-                />
-              </p>
-              <p><input type="submit" value="submit" /></p>
-            </fieldset>
-          </form>
-        </div> : ''}
       </div>
     );
   }
--- src/components/User.js
+++ src/components/User.js
@@ -1,50 +1,39 @@
 import React from "react";
 
 class User extends React.Component {
-
   componentDidMount = () => {
-    this.props.firebase.auth().onAuthStateChanged( user => {
+    this.props.firebase.auth().onAuthStateChanged(user => {
       this.props.setUser(user);
     });
-  }
+  };
 
   signIn = () => {
     const provider = new this.props.firebase.auth.GoogleAuthProvider();
-    this.props.firebase.auth().signInWithPopup( provider );
-  }
+    this.props.firebase.auth().signInWithPopup(provider);
+  };
 
   signOut = () => {
     this.props.firebase.auth().signOut();
-  }
-
+  };
 
   render() {
     return (
       <div>
         <section className="user">
-          <button
-            type="button"
-            className="sign-in"
-            onClick={this.signIn}
-          >
-          Sign In
+          <button type="button" className="sign-in button" onClick={this.signIn}>
+            Sign In
           </button>
-          <button
-            type="button"
-            className="sign-out"
-            onClick={this.signOut}
-            >
-          Sign Out
+          <button type="button" className="sign-out button" onClick={this.signOut}>
+            Sign Out
           </button>
         </section>
-          Current User:
-          {this.props.user ? this.props.user.displayName : 'Guest'}
-        <section>
-        </section>
+        <span className="displayed-name">
+        {this.props.user ? this.props.user.displayName : "Guest"}
+        </span>
+        <section />
       </div>
     );
   }
 }
-
 
 export default User;
