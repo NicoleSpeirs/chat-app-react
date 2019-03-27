9a14f969fd41adc2a0b5edd6f7562c71385db6db
Nicole Speirs <nickispeirs@gmail.com>
2019-03-26 22:59:34 -0500

    fixed sign-in/sign-out button

--- src/components/User.js
+++ src/components/User.js
@@ -20,15 +20,26 @@ class User extends React.Component {
     return (
       <div>
         <section className="user">
-          <button type="button" className="sign-in button" onClick={this.signIn}>
-            Sign In
-          </button>
-          <button type="button" className="sign-out button" onClick={this.signOut}>
-            Sign Out
-          </button>
+          {this.props.user ? (
+            <button
+              type="button"
+              className="sign-out button"
+              onClick={this.signOut}
+            >
+              Sign Out
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="sign-in button"
+              onClick={this.signIn}
+            >
+              Sign In
+            </button>
+          )}
         </section>
         <span className="displayed-name">
-        {this.props.user ? this.props.user.displayName : "Guest"}
+          {this.props.user ? this.props.user.displayName : "Guest"}
         </span>
         <section />
       </div>
